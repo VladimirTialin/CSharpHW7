759430515bbfba0ffbd69c5c71e4c4d1f633a4db
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-06-02 09:53:13 +0300

    Доработал задачу 50

--- Example50v2/Program.cs
+++ Example50v2/Program.cs
@@ -1,95 +1,97 @@
-﻿
 /*Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 Например, задан массив:
 1 4 7 2
 5 9 2 3
 8 4 2 4
-
 17 -> такого числа в массиве нет
 5 -> 9
 */
 //Формируем массив случайных чисел
-
-int[,] NumberArray(int[,] numberArray, int numberRandom)
+int[,] NumberArray(int[,] numberArray)
 {
     for (var i = 0; i < numberArray.GetLength(0); i++)
     {
         for (var j = 0; j < numberArray.GetLength(1); j++)
         {
-            numberArray[i, j] = new Random().Next(numberRandom);
+            numberArray[i, j] = new Random().Next(20,80);
         }
     }
     return numberArray;
 }
-// Выводим массив случайных чисел
+// Выводим массива случайных чисел
 void PrintArray(int[,] arrayToPrint)
 {
     Console.WriteLine();
-    for (var i = 0; i < arrayToPrint.GetLength(0); i++)
-    {
-        for (var j = 0; j < arrayToPrint.GetLength(1); j++)
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {  
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
         {
-            Console.Write(arrayToPrint[i, j] + " ");
+                Console.Write(" " + arrayToPrint[i, j] + " ");
         }
         Console.WriteLine();
     }
+    
 }
 // Функция поиска позиции элемента и вывода значения массива
-int GetIndexArray(int[,] findIndexArray, int positionNumber, int indexRow = 0, int indexColumn = 0)
+int GetIndexArray (int[,] findIndexArray,
+                   int positionNumber)
 {
-    if (positionNumber >= 0 && positionNumber < findIndexArray.Length)
+    if (positionNumber >= 0 &&
+        positionNumber <= findIndexArray.Length-1)
     {
-        indexRow = positionNumber / findIndexArray.GetLength(0);
-        indexColumn = positionNumber / findIndexArray.GetLength(1);
-        return findIndexArray[indexRow, indexColumn];
+        return findIndexArray
+        [positionNumber / findIndexArray.GetLength(1),
+         positionNumber % findIndexArray.GetLength(1)];
     }
     return positionNumber;
 }
 // Красивый вывод массива с результатом
-void ColorPrintArray(int[,] arrayToPrint, int findNumber, int index, bool resetColor = false)
+void ColorPrintArray(int[,] arrayToPrint,int index,
+                     int result=0,int colorIndex=0)
 {
     Console.WriteLine();
-    for (var i = 0; i < arrayToPrint.GetLength(0); i++)
-    {
-        for (var j = 0; j < arrayToPrint.GetLength(1); j++)
+        for (var i = 0; i < arrayToPrint.GetLength(0); i++)
         {
-
-            if (findNumber == arrayToPrint[i, j] && resetColor == false)
+            for (var j = 0; j < arrayToPrint.GetLength(1); j++)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                resetColor = true;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                if (index == colorIndex)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    result = GetIndexArray(arrayToPrint, index);
+                }
+                else
+                {
+                    Console.ResetColor();
+                }
+                Console.Write(" " + arrayToPrint[i, j] + " ");
+                colorIndex++;
             }
-            else
-            {
-                Console.ResetColor();
-            }
-            Console.Write(arrayToPrint[i, j] + " ");
-        }
         Console.WriteLine();
-    }
-    if (findNumber <= 0)
+        }
+    if (index < 0 || index > arrayToPrint.Length - 1)
     {
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"\n {findNumber} - > позиции данного элемента не существует! Повторите ввод.");
+        Console.WriteLine($"\n{index}" +
+        $" - > позиции данного элемента не существует! Повторите ввод.");
         Console.ResetColor();
     }
     else
     {
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"\n{index} -> {findNumber}");
+        Console.WriteLine($"\n В позиции {index} находится значение = {result}");
         Console.ResetColor();
     }
 }
 Console.Write("Введите размер двумерного массива:\nширина m = ");
-int row = Convert.ToInt32(Console.ReadLine());
-Console.Write("длина  n = ");
 int column = Convert.ToInt32(Console.ReadLine());
-var dimensionArray = new int[row, column];
-var numberRandom = new Random().Next(1, 10);
-
-int[,] result = NumberArray(dimensionArray, numberRandom);
+Console.Write("длина  n = ");
+int row= Convert.ToInt32(Console.ReadLine());
+var arrRandom = new int[column,row ];
+int[,] result = NumberArray(arrRandom);
 PrintArray(result);
 Console.Write("\nВыберите позицию элемента массива: ");
 int index = Convert.ToInt32(Console.ReadLine());
-int number = GetIndexArray(result, index);
-ColorPrintArray(result, number, index);
+ColorPrintArray(result, index);
